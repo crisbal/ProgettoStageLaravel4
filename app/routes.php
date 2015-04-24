657c91163a610e924c1061d95806a198ae7a0e80
cris9696 <bld.cris.96@gmail.com>
2015-04-24 16:38:25 +0200

    PhpWord: test

--- app/routes.php
+++ app/routes.php
@@ -24,43 +24,9 @@ Route::get('/aziende/{id}', 'AziendeController@mostraSpecifica');
 
 Route::get('/test', function() {
 
-    $phpWord = new PhpWord();
-
-    // Every element you want to append to the word document is placed in a section.
-    // To create a basic section:
-    $section = $phpWord->addSection();
-
-    // After creating a section, you can append elements:
-    $section->addText('Hello world!');
-
-    // You can directly style your text by giving the addText function an array:
-    $section->addText('Hello world! I am formatted.',
-        array('name'=>'Tahoma', 'size'=>16, 'bold'=>true));
-
-    // If you often need the same style again you can create a user defined style
-    // to the word document and give the addText function the name of the style:
-    $phpWord->addFontStyle('myOwnStyle',
-        array('name'=>'Verdana', 'size'=>14, 'color'=>'ff0000'));
-    $section->addText('Hello world! I am formatted by a user defined style',
-        'myOwnStyle');
-
-    // You can also put the appended element to local object like this:
-    $fontStyle = new \PhpOffice\PhpWord\Style\Font();
-    $fontStyle->setBold(true);
-    $fontStyle->setName('Verdana');
-    $fontStyle->setSize(22);
-    $myTextElement = $section->addText('Hello World!');
-    $myTextElement->setFontStyle($fontStyle);
-
-    // Finally, write the document:
-        // The files will be in your public folder
-    $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
-    $objWriter->save('helloWorld.docx');
-
-    $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'ODText');
-    $objWriter->save('helloWorld.odt');
-
-    $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'RTF');
-    $objWriter->save('helloWorld.rtf');
+    $templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor('documenti/template.docx');
+    $templateProcessor->setValue('nome', htmlspecialchars("Caverio"));
+    $templateProcessor->setValue('cognome', htmlspecialchars("alessandro"));
+    $templateProcessor->saveAs('documenti/compilata.docx');
 
 });
