a132067acdec1cc916526828192d4ed8a2e5bed4
cris9696 <bld.cris.96@gmail.com>
2015-05-04 17:02:49 +0200

    Edit gitignore e output docmenti

--- app/controllers/DocumentiController.php
+++ app/controllers/DocumentiController.php
@@ -47,10 +47,10 @@ class DocumentiController extends BaseController {
 	    $templateProcessor->setValue('tutorScuola_cognome', htmlspecialchars($tutorScuola->cognome));
 
 
-	    $templateProcessor->saveAs('documenti/progettoFormativo-' . $stage->id . '-' . $studente->id . '.docx');
+	    $templateProcessor->saveAs('tmp/progettoFormativo-' . $stage->id . '-' . $studente->id . '.docx');
 
 
-		return Response::download('documenti/progettoFormativo-' . $stage->id . '-' . $studente->id . '.docx');
+		return Response::download('tmp/progettoFormativo-' . $stage->id . '-' . $studente->id . '.docx');
 	}
 
 	public function generaConvenzione($stageId){
@@ -78,8 +78,8 @@ class DocumentiController extends BaseController {
 	    $templateProcessor->setValue('rappresentanteLegale_dataN', htmlspecialchars($rappresentanteLegale->dataN));
 	    $templateProcessor->setValue('rappresentanteLegale_cf', htmlspecialchars($rappresentanteLegale->cf));
 
-		$templateProcessor->saveAs('documenti/convenzione' . '-' . $stage->id . '.docx');
+		$templateProcessor->saveAs('tmp/convenzione' . '-' . $stage->id . '.docx');
 		
-        return Response::download('documenti/convenzione' . '-' . $stage->id . '.docx');	
+        return Response::download('tmp/convenzione' . '-' . $stage->id . '.docx');	
 	}
 }
