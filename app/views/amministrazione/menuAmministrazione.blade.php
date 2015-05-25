1c8b07c8747dc640b5b2a64d871f8793ed8c6a1e
marcoberetta96 <marco.beretta1996@gmail.com>
2015-05-25 20:44:41 +0200

    casino

---
+++ app/views/amministrazione/menuAmministrazione.blade.php
@@ -0,0 +1,49 @@
+@extends('base')
+
+
+@section('title')
+Menu Amministrazione
+@endsection
+
+@section('navAmministrazione')
+active
+@endsection
+
+@section('content')
+<div class="menuAmministrazione">
+	<div class="page-header">
+		<h1> Menù Amministrazione </h1>
+	</div>
+	<!-- INSERIMENTO AZIENDE E ALUNNI-->
+
+    <div class="panel-body">
+	    <div class="row">
+	        <div class="col-lg-6 text-center">
+	        	<a href="{{ action('AmministrazioneController@mostraCreaAziende') }}"><button class="btn btn-lg btn-primary glyphicon glyphicon-briefcase"><br>Inserisci<br>Aziende</button></a>
+	        </div>
+	        <div class="col-lg-6 text-center">
+	        	<a href="{{ action('AmministrazioneController@mostraCreaClasse') }}"><button class="btn btn-lg btn-primary glyphicon glyphicon-user"><br>Inserisci<br>Classe</button></a>
+	        </div>
+	    </div>
+    </div>
+
+	<div class="list-group">
+		<div class="list-group-item list-group-item-info">
+	        <h4>Parametri di configurazione</h4>
+	    </div>
+
+		<div class="list-group-item">
+			<div class="panel-body">
+	    <div class="row">
+	        <div class="col-lg-6 text-center">
+	        	modifica 1 
+	       	</div>
+        	<div class="col-lg-6 text-center">
+        		modifica 2
+    		</div>
+		</div>
+	    <button class="btn btn-primary btn-block">Salva</button>
+    </div>
+	    
+</div>
+@endsection
